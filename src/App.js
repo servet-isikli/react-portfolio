46aab8a4d0162ea154c4f3fb4a9d8c93006d9e51
servet-isikli <servetisikli@yahoo.com>
2023-07-09 16:50:22 +0200

    Learn React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import ShoppingList from "./components/ShoppingList";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ShoppingList />
     </div>
   );
 }
